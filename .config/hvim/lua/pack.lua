c6177d587e9990ec6988dc84c507364ad9e3f905
Heitor Rodrigues <heitor.vrb@gmail.com>
2026-06-09 11:18:45 -0300

    Add ClaudeCode plugin

--- .config/hvim/lua/pack.lua
+++ .config/hvim/lua/pack.lua
@@ -1,6 +1,8 @@
 vim.pack.add({
     "https://github.com/nvim-mini/mini.nvim",
     "https://github.com/kylechui/nvim-surround",
+    "https://github.com/folke/snacks.nvim",
+    "https://github.com/coder/claudecode.nvim",
 })
 
 -- require("mini.files").setup()
@@ -19,8 +21,13 @@ local MiniPick = require("mini.pick")
 MiniPick.setup()
 vim.keymap.set("n", "<leader>pf", function() MiniPick.builtin.files() end, { desc = "Mini File Picker" })
 vim.keymap.set("n", "<leader>ps", function() MiniPick.builtin.grep({ pattern = vim.fn.expand("<cword>") }) end, { desc = "Mini Word Search" })
+vim.keymap.set("n", "<leader>pb", function() MiniPick.builtin.buffers() end, { desc = "Mini Buffer Picker" })
 
 local MiniExtra = require("mini.extra")
 MiniExtra.setup()
 vim.keymap.set("n", "<leader>xx", function() MiniExtra.pickers.diagnostic() end, { desc = "Mini Extra Diagnostic" })
 -- vim.keymap.set("n", "<leader>pk", function() MiniExtra.pickers.keymaps() end, { desc = "Search keymaps" })
+
+require("claudecode").setup({
+    terminal_cmd = "~/.local/bin/claude",
+})

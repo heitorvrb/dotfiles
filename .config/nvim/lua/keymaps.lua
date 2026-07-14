d48d2b9bf030bb411570e84b683200bd3a12bd28
Heitor Rodrigues <heitor.vrb@gmail.com>
2026-07-14 14:38:03 -0300

    nvim Ctrl+BackSpace keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -5,6 +5,8 @@ vim.keymap.set({ "n", "v" }, "<leader>D", [["_dd]], { desc = "Delete whole line 
 
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlighting", silent = true })
 
+vim.keymap.set("i", "<M-BS>", "<C-w>", { desc = "Delete word before cursor" })
+
 vim.keymap.set("n", "<leader>u", function()
     vim.cmd.packadd("nvim.undotree")
     require("undotree").open()

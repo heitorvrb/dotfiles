de0e9bc4be2280ec7f50bbdab8e5d7606bf19686
Heitor Rodrigues <heitor.vrb@gmail.com>
2026-07-14 14:37:48 -0300

    nvim gitsigns keymaps

--- .config/nvim/lua/packs/gitsigns.lua
+++ .config/nvim/lua/packs/gitsigns.lua
@@ -2,7 +2,9 @@ vim.pack.add({
     "https://github.com/lewis6991/gitsigns.nvim",
 })
 
-require("gitsigns").setup({
+local gs = require("gitsigns")
+
+gs.setup({
     signs = {
         add          = { text = "+" },
         delete       = { text = "_", show_count = true },
@@ -13,3 +15,11 @@ require("gitsigns").setup({
         delay = 300,
     },
 })
+
+vim.keymap.set('n', '<leader>hn', gs.next_hunk, { desc = "Next git hunk" })
+vim.keymap.set('n', '<leader>hy', gs.prev_hunk, { desc = "Previous git hunk" })
+vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { desc = "Stage hunk" })
+vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = "Reset hunk" })
+vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = "Preview hunk" })
+vim.keymap.set('n', '<leader>hb', gs.blame_line, { desc = "Blame line" })

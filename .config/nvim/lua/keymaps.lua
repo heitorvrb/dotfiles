8d1884742543c96a509cae2be4bf5c675fe33a4b
Heitor Rodrigues <heitor.vrb@gmail.com>
2026-07-20 16:18:04 -0300

    fix basic keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,7 +3,12 @@ vim.g.mapleader = " "
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
 vim.keymap.set({ "n", "v" }, "<leader>D", [["_dd]], { desc = "Delete whole line without yanking" })
 
-vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlighting", silent = true })
+vim.keymap.set("n", "<leader>n", "<cmd>nohlsearch<CR>", { desc = "Clear search highlighting", silent = true })
+
+vim.keymap.set("n", "<leader>w", function()
+    vim.opt_local.wrap = not vim.opt_local.wrap:get()
+    vim.notify("Wrap: " .. (vim.opt_local.wrap:get() and "ON" or "OFF"))
+end, { desc = "Toggle wrap", silent = true })
 
 vim.keymap.set("i", "<M-BS>", "<C-w>", { desc = "Delete word before cursor" })
 

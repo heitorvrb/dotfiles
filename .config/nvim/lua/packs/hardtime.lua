ea7577654c531d71b87a8412b8f552679b497a4b
Heitor Rodrigues <heitor.vrb@gmail.com>
2026-07-20 16:18:13 -0300

    nvim hardtime setup

--- .config/nvim/lua/packs/hardtime.lua
+++ .config/nvim/lua/packs/hardtime.lua
@@ -3,5 +3,18 @@ vim.pack.add({
 })
 
 require("hardtime").setup({
+    max_count = 4,
     disable_mouse = false,
+    disabled_keys = {
+        ["<Up>"] = false,
+        ["<Down>"] = false,
+        ["<Left>"] = false,
+        ["<Right>"] = false,
+    },
+    restricted_keys = {
+        ["<Up>"] = { "n", "x" },
+        ["<Down>"] = { "n", "x" },
+        ["<Left>"] = { "n", "x" },
+        ["<Right>"] = { "n", "x" },
+    },
 })
